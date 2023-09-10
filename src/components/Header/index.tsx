788d848cbf696cd47ab56c260fd4f8eb1a054eb0
huggoparcelly <h.parcelly@gmail.com>
2023-09-10 16:42:12 -0300

    add header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,7 +7,7 @@ export default function Header() {
             <div className="header-container">
                 <img src={logo} alt="Logo Image" />
                 <h1>Controle de Despesas</h1>
-                <p>Desenvolvido por Huggo Parcelly</p>
+                <p>Desenvolvido por <a href="https://www.linkedin.com/in/huggoparcelly/" target="_blank">Huggo Parcelly</a></p>
             </div>
         </header>
     )
