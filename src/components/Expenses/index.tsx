587e99446f354f9e8c814c230c4fe9ac83e388d6
huggoparcelly <h.parcelly@gmail.com>
2023-09-14 23:53:35 -0300

    add handle submit and handle change to get forms data

--- src/components/Expenses/index.tsx
+++ src/components/Expenses/index.tsx
@@ -1,23 +1,72 @@
 import { Button, Form } from "react-bootstrap";
 
 import './styles.css'
+import { ChangeEvent, FormEvent, useState } from "react";
+import { Expense } from "../../models/expense";
 
 export default function Expenses() {
+
+    const [expensesInfo, setExpensesInfo] = useState<Expense>({
+        description: "",
+        amount: 0.00,
+        type: ""
+    });
+
+    const [formData, setFormData] = useState([] as Expense[]);
+
+    const handleAdd = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        const newData = { ...expensesInfo }
+
+        setFormData([...formData, newData])
+
+        setExpensesInfo({
+            description: "",
+            amount: 0.00,
+            type: ""
+        })
+
+        // verificar a lista
+
+        console.log(formData);
+    }
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.currentTarget;
+
+        setExpensesInfo({
+            ...expensesInfo,
+            [name]: value
+        })
+
+    }
+
     return (
 
-        <Form className="form-container ">
+        <Form className="form-container " onSubmit={handleAdd}>
             <h2>Despesas</h2>
             <div className="inputs-container">
 
                 <Form.Group className="m-3">
                     <Form.Label>Descrição</Form.Label>
-                    <Form.Control type="text" placeholder="Descrição" />
+                    <Form.Control
+                        type="text"
+                        placeholder="Descrição"
+                        name="description"
+                        onChange={handleChange}
+                    />
                 </Form.Group>
 
-                {/* Colocar máscara com a virgula e remover as setas*/}
+                {/* Colocar máscara com a virgula ou ponto*/}
                 <Form.Group className="m-3">
                     <Form.Label>Valor</Form.Label>
-                    <Form.Control type="number" placeholder="R$ 0.00" />
+                    <Form.Control
+                        type="number"
+                        placeholder="R$ 0.00"
+                        name="amount"
+                        onChange={handleChange}
+                    />
                 </Form.Group>
 
             </div>
@@ -29,20 +78,30 @@ export default function Expenses() {
                     type='radio'
                     id='radio-entrada'
                     label='Entrada'
-                    name="group1"
+                    name="type"
+                    value="entrada"
+                    onChange={handleChange}
                 />
                 <Form.Check
                     inline
                     type='radio'
                     id='radio-saida'
                     label='Saida'
-                    name="group1"
                     className="ms-3"
+                    name="type"
+                    value="saida"
+                    onChange={handleChange}
                 />
             </div>
 
             <div className="mb-3">
-                <Button variant="success" className="me-4">Adicionar</Button>
+                <Button
+                    variant="success"
+                    className="me-4"
+                    type="submit"
+                >
+                    Adicionar
+                </Button>
                 <Button variant="danger">Limpar</Button>
             </div>
 
